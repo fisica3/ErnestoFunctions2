44609b819d996a2478b725853cde00c5d517ca1a
Ernesto <ecardenas@rocketmail.com>
2018-10-31 10:10:07 -0500

    probando un nuevo destino

--- src/FunctionAppInVSErnesto/AzureDevOpsTrigger.cs
+++ src/FunctionAppInVSErnesto/AzureDevOpsTrigger.cs
@@ -35,7 +35,7 @@ namespace FunctionAppInVSErnesto
                 return new OkObjectResult($"Go Deploy");
             }
 
-            return new BadRequestObjectResult("Wrongly activation, use different name.");
+            return new BadRequestObjectResult("Wrongly activation, usa un nombre diferente.");
         }
     }
 }
