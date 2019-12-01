cd1e345e4630377e3cc351e902e960ef13966d5b
Ernesto Cárdenas <ernesto.cardenas@gmail.com>
2019-11-30 19:05:02 -0500

    corrigiendo ilog

--- src/functionappinvsernesto/BlobTriggerCSharp.cs
+++ src/functionappinvsernesto/BlobTriggerCSharp.cs
@@ -2,6 +2,7 @@ using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.WebJobs.Extensions.Storage;
+using Microsoft.Extensions.Logging;
 
 namespace FunctionAppInVSErnesto
 {
@@ -9,9 +10,9 @@ namespace FunctionAppInVSErnesto
     public static class BlobTriggerCSharp
     {
         [FunctionName("BlobTriggerCSharp")]
-        public static void Run([BlobTrigger("cookbookfiles/{name}", Connection = "AzureWebJobsStorage")]Stream myBlob, string name, TraceWriter log)
+        public static void Run([BlobTrigger("cookbookfiles/{name}", Connection = "AzureWebJobsStorage")]Stream myBlob, string name, ILogger log)
         {
-            log.Info($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
         }
     }
 }
