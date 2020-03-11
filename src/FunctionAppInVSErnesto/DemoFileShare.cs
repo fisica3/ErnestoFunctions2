403f3742a5e6530fed23a6e2052a09eb4a9f2ceb
Ernesto <ecardenas@rocketmail.com>
2020-03-11 18:20:55 -0500

    Limpiando comentarios

--- src/FunctionAppInVSErnesto/DemoFileShare.cs
+++ src/FunctionAppInVSErnesto/DemoFileShare.cs
@@ -1,17 +1,11 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Azure.Storage;
 using Azure.Storage.Blobs;
-using Azure.Storage.Blobs.Models;
 using Azure.Storage.Files.Shares;
-using Azure.Storage.Files.Shares.Models;
-using Newtonsoft.Json;
 
 namespace FunctionAppInVSErnesto
 {
@@ -27,10 +21,6 @@ namespace FunctionAppInVSErnesto
             string shareName = req.Query["sharename"];
             string fileName = req.Query["filename"];
 
-            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            //dynamic data = JsonConvert.DeserializeObject(requestBody);
-            //shareName = shareName ?? data?.shareName;
-
             if (shareName != null && fileName!=null)
             {
                 log.LogInformation($"OJO!! El nombre del Share es: {shareName}, y el nombre de file es {fileName}");
@@ -41,22 +31,8 @@ namespace FunctionAppInVSErnesto
             else return new BadRequestObjectResult("Por favor pase los parametros en el querystring");
         }
 
-        /// <summary>
-        /// Download a file.
-        /// </summary>
-        /// <param name="connectionString">
-        /// A connection string to your Azure Storage account.
-        /// </param>
-        /// <param name="shareName">
-        /// The name of the share to download from.
-        /// </param>
-        /// <param name="localFilePath">
-        /// Path to download the local file.
-        /// </param>
         public static void DownloadAndCopy(string connectionString, string shareName, string fileName, ILogger log)
         {
-            #region Snippet:Azure_Storage_Files_Shares_Samples_Sample01a_HelloWorld_Download
-
             string dirName = "prueba-dir";
 
             // Get a reference to the file
@@ -65,26 +41,16 @@ namespace FunctionAppInVSErnesto
             ShareFileClient file = directory.GetFileClient(fileName);
 
             var uriFile = file.Uri;
+            //Parametrizar
             var containerName = "destino";           
 
             BlobContainerClient container = new BlobContainerClient(connectionString, containerName);
-          //  container.Create();
-
-            // Get a reference to a blob named "sample-file" in a container named "sample-container"
             BlobClient blob = container.GetBlobClient(fileName);
 
             // Upload local file
             
             blob.Upload(file.Download().Value.Content);
             log.LogInformation($"Archivo {fileName} copiado en container {containerName}");
-            /*
-            // Download the file
-            ShareFileDownloadInfo download = file.Download();
-            using (FileStream stream = File.OpenWrite(localFilePath))
-            {
-                download.Content.CopyTo(stream);
-            } */
-            #endregion Snippet:Azure_Storage_Files_Shares_Samples_Sample01a_HelloWorld_Download
         }
                
     }
