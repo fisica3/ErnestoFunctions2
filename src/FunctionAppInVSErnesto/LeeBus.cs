27a2cfc0c2495b738b91ef4e14a797d4d409843d
Ernesto Cárdenas <ernesto.cardenas@gmail.com>
2020-06-09 12:21:14 -0500

    actualizando con el uso de app config

--- src/FunctionAppInVSErnesto/LeeBus.cs
+++ src/FunctionAppInVSErnesto/LeeBus.cs
@@ -4,10 +4,19 @@ using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using System.Text;
+using Microsoft.Extensions.Configuration;
+
 namespace FunctionAppInVSErnesto
 {
     public static class LeeBus
     {
+        private static IConfiguration Configuration { set; get; }
+        static LeeBus()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("ejemplobus2000"));
+            Configuration = builder.Build();
+        }
         [FunctionName("LeeBus")]
         public static void Run([ServiceBusTrigger("ejemploacp", "Medellin", Connection = "ejemplobus2000")]Message mySbMsg, ILogger log)
         {
--- src/functionappinvsernesto/HTTPTriggerCSharpFromVS.cs
+++ src/functionappinvsernesto/HTTPTriggerCSharpFromVS.cs
@@ -1,33 +1,63 @@
 
+using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 namespace FunctionAppInVSErnesto
 {
-    public static class HTTPTriggerCSharpFromVS
+    public static class HttpTriggerCSharpFromVs
     {
+        private static IConfiguration Configuration { set; get; }
+        private static IConfigurationRefresher ConfigurationRefresher { set; get; }
+
+        static HttpTriggerCSharpFromVs()
+        {
+            var builder = new ConfigurationBuilder();
+            var cadena = Environment.GetEnvironmentVariable("ConnectionString");
+            builder.AddAzureAppConfiguration(options =>
+            {
+                options.Connect(cadena)
+                    .ConfigureRefresh(refreshOptions =>
+                        refreshOptions.Register("TestApp:Settings:Message")
+                            .SetCacheExpiration(TimeSpan.FromSeconds(60))
+                    );
+                ConfigurationRefresher = options.GetRefresher();
+            });
+            Configuration = builder.Build();
+        }
+
         [FunctionName("HTTPTriggerCSharpFromVS")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            await ConfigurationRefresher.RefreshAsync();
+            string keyName = "TestApp:Settings:Message";
+            string message = Configuration[keyName];
 
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            /* string name = req.Query["name"];
 
-            if (name != null)
-            {
-                log.LogInformation($"OJO!! El valor capturado es: {name}");
-                return (ActionResult)new OkObjectResult($"Hello, {name}");
-            }
-            else return new BadRequestObjectResult("Por favor pase el parametro name en el querystring o en el body del POST");
+             string requestBody = new StreamReader(req.Body).ReadToEnd();
+             dynamic data = JsonConvert.DeserializeObject(requestBody);
+             name = name ?? data?.name;
+
+             if (name != null)
+             {
+                 log.LogInformation($"OJO!! El valor capturado es: {name}");
+                 return (ActionResult)new OkObjectResult($"Hello, {name}");
+             }
+             else return new BadRequestObjectResult("Por favor pase el parametro name en el querystring o en el body del POST"); */
+
+            return message != null
+                ? (ActionResult)new OkObjectResult(message)
+                : new BadRequestObjectResult($"Please create a key-value with the key '{keyName}' in App Configuration.");
         }
     }
 }
