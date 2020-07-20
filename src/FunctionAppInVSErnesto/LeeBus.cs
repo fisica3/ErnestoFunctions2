5bc03b324aa5f71c53b40cf2d0a5e5eb3539cf45
Ernesto Cardenas <ecardenas@rocketmail.com>
2020-07-20 16:10:58 -0500

    agregando cambios con App Configuration

--- src/FunctionAppInVSErnesto/LeeBus.cs
+++ src/FunctionAppInVSErnesto/LeeBus.cs
@@ -5,7 +5,7 @@ using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using System.Text;
 using Microsoft.Extensions.Configuration;
-
+using Azure.Identity;
 namespace FunctionAppInVSErnesto
 {
     public static class LeeBus
@@ -14,15 +14,37 @@ namespace FunctionAppInVSErnesto
         static LeeBus()
         {
             var builder = new ConfigurationBuilder();
-            builder.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("ejemplobus2000"));
+            bool isLocal = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID"));
+
+            if (isLocal)
+            {
+                builder.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("KeyConnectionString"));
+            }
+            else
+            {
+                builder.AddAzureAppConfiguration(options =>
+                    options.Connect(new Uri(Environment.GetEnvironmentVariable("Endpoint")), new ManagedIdentityCredential()));
+            }
             Configuration = builder.Build();
+            
+           /* catch (CredentialUnavailableException e)
+            {
+                builder = new ConfigurationBuilder();
+                
+                Configuration = builder.Build();
+            }*/
+            
         }
         [FunctionName("LeeBus")]
         public static void Run([ServiceBusTrigger("ejemploacp", "Medellin", Connection = "ejemplobus2000")]Message mySbMsg, ILogger log)
         {
             log.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg.MessageId}");
+
+            string keyName = "TestApp:Settings:Message";
+            string message = Configuration[keyName];
+
             var content = Encoding.ASCII.GetString(mySbMsg.Body, 0, mySbMsg.Body.Length); 
-            log.LogInformation(content);
+            log.LogInformation($"Desde SB: {content}. Desde AppConfig: {message}");
         }
     }
 }
