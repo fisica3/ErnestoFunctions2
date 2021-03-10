f4290cbe00d71616486a93358a71380e73d3deaa
Ernesto Cardenas <ecardenas@rocketmail.com>
2021-03-10 11:21:08 -0500

    subiendo cambio para refresh

--- src/functionappinvsernesto/TestAppConfig.cs
+++ src/functionappinvsernesto/TestAppConfig.cs
@@ -42,19 +42,19 @@ namespace FunctionAppInVSErnesto
             log.LogInformation("El trigger HTTP con C#, proceso un request.");
             string keyVaultEntry = "proxymusk";
             string messageKeyVault = "keyvault es local";
-            
+            await _configurationRefresher.TryRefreshAsync();
             if (!isLocal)
             {
                 await _configurationRefresher.RefreshAsync();
                 messageKeyVault = _configuration[keyVaultEntry];
             }
             
-            bool flag = await _featureManagerSnapshot.IsEnabledAsync("ActivacionMensaje");
+            //bool flag = await _featureManagerSnapshot.IsEnabledAsync("ActivacionMensaje");
             string keyName =  "TestApp:Settings:Message02";
             string message = _configuration[keyName];
             
             return message != null
-                ? (ActionResult)new OkObjectResult($"El valor recuperado desde AppConfig fue '{message}', y el valor desde KeyVault fue '{messageKeyVault}' el FeatureFlag devolvio {flag.ToString()} en el repo")
+                ? (ActionResult)new OkObjectResult($"El valor recuperado desde AppConfig fue '{message}', y el valor desde KeyVault fue '{messageKeyVault}' la operacion funciono bien en Azure.")
                 : new BadRequestObjectResult($"Please create a key-value with the key '{keyName}' in App Configuration, gracias.");
         }
     }
