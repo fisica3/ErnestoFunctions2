3d78115bc87d147ee09eed3ec969218e7d8d2c68
Ernesto Cardenas <ecardenasc@intercorp.com.pe>
2022-01-17 20:51:13 -0500

    Regresando el Durable

--- src/functionappinvsernesto/DemoDurable.cs
+++ src/functionappinvsernesto/DemoDurable.cs
@@ -18,20 +18,20 @@ namespace FunctionAppInVSErnesto
             var outputs = new List<string>();
 
             // Replace "hello" with the name of your Durable Activity Function.
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "Tokyo",5));
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "Seattle", 1));
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "London"), 3);
+            outputs.Add(await context.CallActivityAsync<string>("DemoDurable_Hello", "Tokyo"));
+            outputs.Add(await context.CallActivityAsync<string>("DemoDurable_Hello", "Seattle"));
+            outputs.Add(await context.CallActivityAsync<string>("DemoDurable_Hello", "London"));
 
             // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
             return outputs;
         }
 
-        [FunctionName("Function1_Hello")]
-        public static string SayHello([ActivityTrigger] string name, ILogger log , int delay)
+        [FunctionName("DemoDurable_Hello")]
+        public static string SayHello([ActivityTrigger] string name, ILogger log)
         {
-            //    var rnd = new System.Random(System.DateTime.Now.Millisecond);
-            //    int a = 0;
-            int bucle = 1000 * delay;// rnd.Next(4);
+            var rnd = new System.Random(System.DateTime.Now.Millisecond);
+            //int a = 0;
+            int bucle = 1000 * rnd.Next(4);
             for (int i = 0; i < bucle; i++) //1580200
             {
                 log.LogInformation($"Contador {i}");
@@ -40,10 +40,10 @@ namespace FunctionAppInVSErnesto
             return $"Hello {name}!";
         }
 
-        [FunctionName("Function1_HttpStart")]
+        [FunctionName("DemoDurable_HttpStart")]
         public static async Task<HttpResponseMessage> HttpStart(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]HttpRequestMessage req,
-            [OrchestrationClient]IDurableOrchestrationClient starter,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
+            [DurableClient] IDurableOrchestrationClient starter,
             ILogger log)
         {
             // Function input comes from the request content.
